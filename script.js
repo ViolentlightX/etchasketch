1c5d2645e10793be0a4a7c5c68b1c20601313b28
ViolentlightX <35661665+ViolentlightX@users.noreply.github.com>
2022-12-27 17:15:00 -0700

    Started grid creation by user Only creating 2 divs in grid, must resolve

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-//Get grid size value. Display value. Return grid size value
+// Get grid size value. Display value. Return grid size value
 const setGridSize = () => {
     const userRangeSelect = document.querySelector("#sizeSlider");
+    const mainDrawArea = document.querySelector("#mainDrawArea");
+    const gridBlock = document.createElement("div");
+    gridBlock.classList.add('grid-item');
 
     userRangeSelect.addEventListener("input", e => {
         const rangeValueDisplay = document.querySelector(".gridSizeDisplayValue");
-        rangeValueDisplay.innerHTML = `${e.target.value} X ${e.target.value}`;
+        const gridSize = e.target.value;
+
+        rangeValueDisplay.innerHTML = `${gridSize} X ${gridSize}`;
+
+        mainDrawArea.setAttribute("grid-template-columns", "repeat(" + gridSize + ", auto)");
+    
+        for (i = 0; i < gridSize ** 2; i++) {
+            mainDrawArea.append(gridBlock);
+        }
     })
     return e.target.value;
 }
 
+// Get brush color from interface
 const setBrushColor = () => {
     const userColorSelect = document.querySelector("#colorPicker");
 
@@ -16,3 +28,5 @@ const setBrushColor = () => {
         return e.target.value;
     })
 }
+
+setGridSize();
